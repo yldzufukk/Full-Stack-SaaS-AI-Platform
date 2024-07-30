2f6159285db6a38a7cf8ab1ae8c54a8df86f3601
yldzufukk <84462888+yldzufukk@users.noreply.github.com>
2024-07-30 15:32:56 +0300

    music generation ai waiting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //images: {
+    //    domains: [
+    //        "domain1",
+    //    ]
+    //}
+};
 
 export default nextConfig;
